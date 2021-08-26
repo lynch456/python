e118b9e1f47a9c25d8b6ca4381869c352ba3cf8d
Kim Ik-Soo <87025474+lynch456@users.noreply.github.com>
2021-08-26 23:13:41 +0900

    8/26 format, control

---
+++ day04/variable2.py
@@ -0,0 +1,22 @@
+
+# print("이름 : ", end="")
+# print(name)
+# print("나이 : ", end="")
+# print(age, end="살\n")
+# print("키 : ", end="")
+# print(height, end="cm\n")
+# print("취미 : ", end="")
+# print(hobby)
+
+# 서식문자(format)
+# 반드시 따옴표 안에서 작성한다.
+# %d정수  %f실수  %c문자  %s문자열
+name = "kimiksoo"
+age = 10
+height = 120.123
+hobby = "piano"
+
+print("이름 : %s" % name)
+print("이름 : %d" % age)
+print("이름 : %.3fcm" % height)
+print("이름 : %s" % hobby)
